3072a454bac87c4e7726ab947d44a6c49c36254f
frazzex <rtmtolokonnikov@gmail.com>
2025-12-13 23:04:23 +0700

    [ADD] Добавление поля is_admin для модели пользователя

--- models.py
+++ models.py
@@ -1,17 +1,17 @@
 from tortoise.models import Model
 from tortoise import fields
-from typing import Optional
 
 
 class User(Model):
     """Модель пользователя"""
-    id = fields.BigIntField(pk=True)  # Telegram user ID
+    id = fields.BigIntField(pk=True)
     username = fields.CharField(max_length=255, null=True)
     full_name = fields.CharField(max_length=255, null=True)
-    subgroup = fields.IntField(null=True)  # 1 или 2 подгруппа
-    group = fields.CharField(max_length=50, null=True)  # Название группы
+    subgroup = fields.IntField(null=True)
+    group = fields.CharField(max_length=50, null=True)
     created_at = fields.DatetimeField(auto_now_add=True)
     updated_at = fields.DatetimeField(auto_now=True)
+    is_admin = fields.BooleanField(default=False)
 
     class Meta:
         table = "users"
