110eaad3ed0963415f61d599065b7503f38306e6
frazzex <148036637+frazzex@users.noreply.github.com>
2025-12-18 21:10:19 +0700

    [REM] Удаление полей group и is_admin из модели user
    Co-authored-by: gard762 <markvoytovich807@gmail.com>

--- models/User.py
+++ models/User.py
@@ -6,10 +6,8 @@ class User(Model):
     username = fields.CharField(max_length=255, null=True)
     full_name = fields.CharField(max_length=255, null=True)
     subgroup = fields.IntField(null=True)
-    group = fields.CharField(max_length=50, null=True)
     created_at = fields.DatetimeField(auto_now_add=True)
     updated_at = fields.DatetimeField(auto_now=True)
-    is_admin = fields.BooleanField(default=False)
 
     class Meta:
         table = "users"
